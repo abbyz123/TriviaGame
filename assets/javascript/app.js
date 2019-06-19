7129bec3b2f129a5db63658321ad100debad84fb
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-18 21:33:23 -0700

    add click event handler for start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,22 @@
+let triviaQuiz = [{question : 'who is your daddy?',
+                   choice   : ['you are', 'he is', 'no one is'],
+                   answer   : 1}];
+
+let currQuizIdx = 0;
+
 $(function () {
-    // add trivia zone styling to jumbotron
+    // add css styling
     $('#triviaGameZone').addClass('triviaZone');
-
-    // center the header and question container
     $('h1').addClass('center');
-
-    // set button margin
     $("#start").addClass("startButton");
+    $("#questionZone").addClass("questionText");
+
+    // TODO: shuffle the sequence of the quiz questions
+
+    // on click the start button, quiz starts!
+    $("#start").on("click", function(e) {
+        console.log('start');
+        // show question
+        $("#questionZone").text(triviaQuiz[currQuizIdx].question);
+    })
 });
