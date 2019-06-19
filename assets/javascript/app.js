3e101ca4243adf45fc2fafac128c7f36f490ec03
mengyuan zhang <zhangm8976@student.laccd.edu>
2019-06-18 21:14:08 -0700

    initial webpage structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+$(function () {
+    // add trivia zone styling to jumbotron
+    $('#triviaGameZone').addClass('triviaZone');
+
+    // center the header and question container
+    $('h1').addClass('center');
+
+    // set button margin
+    $("#start").addClass("startButton");
+});
